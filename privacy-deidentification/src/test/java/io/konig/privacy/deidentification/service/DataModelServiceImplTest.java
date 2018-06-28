4b90b185f321332e489b71780acaf3bc02d1f391
SriramMani1 <sriram.mani@pearson.com>
2018-06-28 14:55:41 +0530

    Closes #7

---
+++ privacy-deidentification/src/test/java/io/konig/privacy/deidentification/service/DataModelServiceImplTest.java
@@ -0,0 +1,90 @@
+package io.konig.privacy.deidentification.service;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.konig.privacy.deidentification.repo.DataModelRepository;
+
+@RunWith(SpringRunner.class)
+public class DataModelServiceImplTest {
+
+	@TestConfiguration
+    static class DataModelServiceImplTestContextConfiguration {
+  
+        @Bean
+        public DataModelService dataModelService() {
+            return new DataModelServiceImpl();
+        }
+    }
+	
+	@Autowired
+    private DataModelService dataModelService;
+ 
+    @MockBean
+    private DataModelRepository dataModelRepository;
+    
+    @Before
+    public void setUp() throws JsonProcessingException, IOException, DataAccessException {
+    	String jsonStr = "{\n "+
+   "  \"title\" : \"Person\", \n" +
+   "  \"type\" : \"object\", \n" +
+   "  \"properties\" : { \n" +
+   "  \"givenName\" : { \n" +
+   "   \"type \" : \"string\" \n" +
+   "   }, \n" +
+   "  \"familyName\" : { \n" +
+   "  \"type\" : \"string\" \n" +
+   "  }, \n" +
+   "  \"email\" : { \n" +
+   "  \"type \" : \"array\", \n"+
+   "  \"items \" : { \n" +
+   "  \"type\" : \"string\" \n" +
+   "  } \n" +
+   "  } \n" +
+   "  } \n" +
+   "  } \n";
+    
+    	ObjectMapper mapper = new ObjectMapper();
+		JsonNode jsonNode = mapper.readValue(jsonStr, JsonNode.class);
+		
+		System.out.println("jsonNode in set up method" + jsonNode.get("title").textValue());
+		
+		 Mockito.when(dataModelRepository.getSchemaByVersion("V1"))
+         .thenReturn(jsonNode);
+		 
+		 Mockito.when(dataModelRepository.getLatestDataModel())
+         .thenReturn("V1");
+		 
+		 Mockito.when(dataModelRepository.dataModelExists("V1"))
+		 .thenReturn(true);
+    }
+    
+    @Test
+    public void getSchemaByVersion() throws DataAccessException, JsonProcessingException, IOException {
+    	JsonNode jsonNode= dataModelService.getSchemaByVersion("V1");
+    	assertNotNull(jsonNode);
+    	assertEquals("Person",jsonNode.path("title").textValue());    	
+    }
+    
+    @Test
+    public void getlatestDataModel() throws DataAccessException, JsonProcessingException, IOException {
+    	String version= dataModelService.getLatestDataModel();
+    	assertNotNull(version);
+    	assertEquals("V1",version);
+    }
+}
