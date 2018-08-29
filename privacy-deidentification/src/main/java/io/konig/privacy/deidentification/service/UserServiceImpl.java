5f4b8e8921f4d0af4ee04c9a1b6f694a3e37a029
ElakiyaUdhayanan <elakiya.udhayanan@pearson.com>
2018-08-29 11:32:02 +0530

    partial fixes

--- privacy-deidentification/src/main/java/io/konig/privacy/deidentification/service/UserServiceImpl.java
+++ privacy-deidentification/src/main/java/io/konig/privacy/deidentification/service/UserServiceImpl.java
@@ -2,11 +2,13 @@ package io.konig.privacy.deidentification.service;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import io.konig.privacy.deidentification.model.Users;
 import io.konig.privacy.deidentification.repo.UserRepository;
-
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
