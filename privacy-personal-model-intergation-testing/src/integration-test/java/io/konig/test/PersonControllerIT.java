d414d52546ec7f3d38b2f27971173e989728d9e7
SriramMani1 <sriram.mani@pearson.com>
2018-09-06 14:38:05 +0530

    closes #17

--- privacy-personal-model-intergation-testing/src/integration-test/java/io/konig/test/PersonControllerIT.java
+++ privacy-personal-model-intergation-testing/src/integration-test/java/io/konig/test/PersonControllerIT.java
@@ -3,12 +3,15 @@ package io.konig.test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -25,7 +28,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 
-//import io.konig.privacy.deidentification.Application;
+import io.konig.privacy.deidentification.Application;
 
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.DefaultInvoker;
@@ -33,9 +36,13 @@ import org.apache.maven.shared.invoker.InvocationRequest;
 import org.apache.maven.shared.invoker.InvocationResult;
 
 import java.util.Collections;
+import java.util.Scanner;
 
 import com.google.common.io.Files;
 
+import io.konig.privacy.deidentification.rest.PersonController;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
  
 
 
@@ -45,13 +52,12 @@ import com.google.common.io.Files;
  */
 @RunWith(SpringRunner.class)
 @Category(IntegrationTest.class)
-//Commented Spring boot Test annotation, as it works only when configuration of Spring boot maven plugin goal as exec in privacy-personal-model pom.xml
-//@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc(secure=false)
 public class PersonControllerIT 
 {
-	//@Autowired
-	//private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 	
 	
 	@Autowired
@@ -59,79 +65,94 @@ public class PersonControllerIT
 	
 	private static File mavenHome = null;
 	
-	@Test
+	@Ignore
 	public void postSensitivePII() throws Exception {
 		
-		System.out.println("In post Sensistive PII");
-		//This option is using maven invoker to start sping boot. It is not able to find Spring boot Application class.
-		InvocationRequest request = new DefaultInvocationRequest();
-		request.setPomFile(new File("D:/Sriram/konig-privacy/privacy-personal-model-intergation-testing/src/integration-test/resources/pom.xml"));
-		request.setGoals(Collections.singletonList("spring-boot:run"));
-		DefaultInvoker invoker = invoker();
-		InvocationResult result = invoker.execute(request);
-		assertTrue(result.getExitCode() != 0 ? result.getExecutionException().toString() : "Success",
-				result.getExitCode() == 0);
-		
-		//below option uses sping boot annotation to invoke the service
-		/*ObjectMapper mapper = new ObjectMapper();
-		
-		JsonNode node=mapper.readTree(new File("personRequest.json"));
+		ObjectMapper mapper = new ObjectMapper();
+		String workingdir=System.getProperty("user.dir");
+		JsonNode node=mapper.readTree(new File(workingdir+"/src/integration-test/resources/personRequest.json"));
 		RequestBuilder requestBuilder = MockMvcRequestBuilders
-				.post("/privacy/v5/person")
+				.post("/api/privacy/v5/person")
 				.accept(MediaType.APPLICATION_JSON).content(node.toString())
 				.contentType(MediaType.APPLICATION_JSON);
 		
 		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 		
 		MockHttpServletResponse response = result.getResponse();
-		
-		System.out.println("Response from Service===>"+response.getStatus());
-		
-		assertEquals(HttpStatus.CREATED.value(), response.getStatus());*/
-		
-	}
-	
-	private DefaultInvoker invoker() {
-		DefaultInvoker invoker = new DefaultInvoker();
-		File mavenHome = mavenHome();
-		invoker.setMavenHome(mavenHome);
-		return invoker;
-	}
-	
-	private File mavenHome() {
-		if (mavenHome == null) {
-		System.out.println("Path environment"+System.getenv("PATH"));
-			for (String dirname : System.getenv("PATH").split(File.pathSeparator)) {
-				File file = new File(dirname, "mvn");
-				if (file.isFile()) {
-					mavenHome = file.getParentFile().getParentFile();
-					createBatchFile();
-					return mavenHome;
-				}
-			}
-			throw new RuntimeException("Maven executable not found.");
-		}
-		return mavenHome;
-	}
-	
-	private void createBatchFile() {
-		
-		String os = System.getProperty("os.name").toLowerCase();
-		if (os.contains("win")) {
-			// We are running on a Windows machine.  Copy the file.
-			File batFile = new File(mavenHome, "bin/mvn.bat");
-			if (!batFile.exists()) {
-				File cmdFile = new File(mavenHome, "bin/mvn.cmd");
-				if (cmdFile.exists()) {
-					try {
-						Files.copy(cmdFile, batFile);
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
-				}
 				
-			}
-		}
+		assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+		
+		String annotatedPersonExpected = loadAsString(workingdir+"\\src\\integration-test\\resources\\expectedAnnotatedPerson.json");
+				
+		assertEquals(annotatedPersonExpected,response.getContentAsString());
+				
+				
+		
+	}
+	
+	@Test
+	public void integrationTestGBI() throws Exception {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String workingdir=System.getProperty("user.dir");
+		JsonNode dataModelNode=mapper.readTree(new File(workingdir+"/src/integration-test/resources/GBIDataModelRequest.json"));
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post("/api/schema")
+				.accept(MediaType.APPLICATION_JSON).content(dataModelNode.toString())
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult dataModelresult = mockMvc.perform(requestBuilder).andReturn();
+		
+		MockHttpServletResponse dataModelresponse = dataModelresult.getResponse();
+		
+		assertEquals(HttpStatus.CREATED.value(), dataModelresponse.getStatus());
+		
+		String version= (String) dataModelresponse.getHeaderValue("Location");
+						
+		ObjectMapper personMapper = new ObjectMapper();		
+		JsonNode personnode=personMapper.readTree(new File(workingdir+"/src/integration-test/resources/GBIPersonRequest.json"));
+		RequestBuilder personRequestBuilder = MockMvcRequestBuilders
+				.post("/api/privacy/"+version.substring(8)+"/person")
+				.accept(MediaType.APPLICATION_JSON).content(personnode.toString())
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult personResult = mockMvc.perform(personRequestBuilder).andReturn();
+		
+		MockHttpServletResponse personResponse = personResult.getResponse();
+			
+		assertEquals(HttpStatus.CREATED.value(), personResponse.getStatus());
+		
+		ObjectMapper personResponseMapper=new ObjectMapper();
+		
+		JsonNode personresponseNode= personResponseMapper.readTree(personResponse.getContentAsString());
+		
+		String pseudonym=personresponseNode.findValue("pseudonym").textValue();
+		
+		RequestBuilder annotatedPersonRequestBuilder = MockMvcRequestBuilders
+				.get("/api/privacy/"+version.substring(8)+"/person/"+pseudonym+"/.annotated")
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult annotatedPersonResult = mockMvc.perform(annotatedPersonRequestBuilder).andReturn();
+		
+		MockHttpServletResponse annotatedpersonResponse = annotatedPersonResult.getResponse();
+		
+		assertEquals(HttpStatus.OK.value(), annotatedpersonResponse.getStatus());
+		
+		
+	}
+	
+	private String loadAsString(String path) throws FileNotFoundException{
+		System.out.println("Path"+path);
+		InputStream input = new FileInputStream(path);
+		System.out.println("input"+input);
+		/*if (input == null) {
+			throw new RuntimeException("Resource not found: " + path);
+		}*/
+		Scanner scanner = new Scanner(input, "UTF-8");
+		scanner.useDelimiter("\\A");
+		String result = scanner.hasNext() ? scanner.next() : "";
+		scanner.close();
+		return result;		
 		
 	}
     
