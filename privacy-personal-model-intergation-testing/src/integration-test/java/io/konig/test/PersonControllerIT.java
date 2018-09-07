608c8078ad594e6aed484e1ea232256179f54414
SriramMani1 <sriram.mani@pearson.com>
2018-09-07 14:27:54 +0530

    Closes #17

--- privacy-personal-model-intergation-testing/src/integration-test/java/io/konig/test/PersonControllerIT.java
+++ privacy-personal-model-intergation-testing/src/integration-test/java/io/konig/test/PersonControllerIT.java
@@ -26,6 +26,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+
 import java.io.File;
 
 import io.konig.privacy.deidentification.Application;
@@ -35,7 +39,9 @@ import org.apache.maven.shared.invoker.DefaultInvoker;
 import org.apache.maven.shared.invoker.InvocationRequest;
 import org.apache.maven.shared.invoker.InvocationResult;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.common.io.Files;
@@ -74,7 +80,7 @@ public class PersonControllerIT
 		RequestBuilder requestBuilder = MockMvcRequestBuilders
 				.post("/api/privacy/v5/person")
 				.accept(MediaType.APPLICATION_JSON).content(node.toString())
-				.contentType(MediaType.APPLICATION_JSON);
+				.contentType(MediaType.APPLICATION_JSON).header("Authorization", "Basic dGVzdDp0ZXN0MTIz");
 		
 		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 		
@@ -92,14 +98,15 @@ public class PersonControllerIT
 	
 	@Test
 	public void integrationTestGBI() throws Exception {
-		
 		ObjectMapper mapper = new ObjectMapper();
 		String workingdir=System.getProperty("user.dir");
+				
+		
 		JsonNode dataModelNode=mapper.readTree(new File(workingdir+"/src/integration-test/resources/GBIDataModelRequest.json"));
 		RequestBuilder requestBuilder = MockMvcRequestBuilders
 				.post("/api/schema")
 				.accept(MediaType.APPLICATION_JSON).content(dataModelNode.toString())
-				.contentType(MediaType.APPLICATION_JSON);
+				.contentType(MediaType.APPLICATION_JSON).header("Authorization", "Basic dGVzdDp0ZXN0MTIz");
 		
 		MvcResult dataModelresult = mockMvc.perform(requestBuilder).andReturn();
 		
@@ -114,7 +121,7 @@ public class PersonControllerIT
 		RequestBuilder personRequestBuilder = MockMvcRequestBuilders
 				.post("/api/privacy/"+version.substring(8)+"/person")
 				.accept(MediaType.APPLICATION_JSON).content(personnode.toString())
-				.contentType(MediaType.APPLICATION_JSON);
+				.contentType(MediaType.APPLICATION_JSON).header("Authorization", "Basic dGVzdDp0ZXN0MTIz");
 		
 		MvcResult personResult = mockMvc.perform(personRequestBuilder).andReturn();
 		
@@ -128,9 +135,26 @@ public class PersonControllerIT
 		
 		String pseudonym=personresponseNode.findValue("pseudonym").textValue();
 		
+		assertNotNull(pseudonym);
+		
+		String email=null;
+		
+		JsonNode personNode = personresponseNode.path("data");
+		ArrayNode emailArray = (ArrayNode)personresponseNode.findValue("email");
+		for (int j = 0; j < emailArray.size(); j++) {
+			TextNode objectValue =  (TextNode) emailArray.get(j);				
+			email=objectValue.asText();
+		}		
+			
+		
+		assertNotNull(email);
+		
+		assertEquals("Tanji@example.com",email);
+				
+	
 		RequestBuilder annotatedPersonRequestBuilder = MockMvcRequestBuilders
 				.get("/api/privacy/"+version.substring(8)+"/person/"+pseudonym+"/.annotated")
-				.contentType(MediaType.APPLICATION_JSON);
+				.contentType(MediaType.APPLICATION_JSON).header("Authorization", "Basic dGVzdDp0ZXN0MTIz");
 		
 		MvcResult annotatedPersonResult = mockMvc.perform(annotatedPersonRequestBuilder).andReturn();
 		
@@ -138,16 +162,27 @@ public class PersonControllerIT
 		
 		assertEquals(HttpStatus.OK.value(), annotatedpersonResponse.getStatus());
 		
+		ObjectMapper annotatedPersonMapper = new ObjectMapper();
 		
+		JsonNode annotationData = annotatedPersonMapper.readTree(annotatedpersonResponse.getContentAsString());
+		
+		ArrayNode annotationArray= (ArrayNode) annotationData.get("graph");
+		boolean emailflag=false;
+		for (int i=0; i<annotationArray.size(); i++) {
+			JsonNode node = annotationArray.get(i);
+			String propertyName = node.get("property").asText();
+			String emailValue= node.get("value").asText();
+			if ("email".equals(propertyName) && emailValue.equals("Tanji@example.com")) {
+				emailflag=true;
+				break;
+			}
+		}
+		assertTrue(emailflag);
+				
 	}
 	
 	private String loadAsString(String path) throws FileNotFoundException{
-		System.out.println("Path"+path);
 		InputStream input = new FileInputStream(path);
-		System.out.println("input"+input);
-		/*if (input == null) {
-			throw new RuntimeException("Resource not found: " + path);
-		}*/
 		Scanner scanner = new Scanner(input, "UTF-8");
 		scanner.useDelimiter("\\A");
 		String result = scanner.hasNext() ? scanner.next() : "";
